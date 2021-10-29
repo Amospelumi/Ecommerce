6f0c3bd846c64bc28c5f0b396a630a09e6a69eae
Amospelumi <oluwapelumiabiodun45@gmail.com>
2021-10-29 11:32:09 +0100

    Ecommerce

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/store.css">
+    <title>Online-Store</title>
+
+    <style>
+        .container{
+            position: relative;
+            height: 700px;
+            background-image: linear-gradient(#DA1396, #da139475), url('img/bg.jpeg'); 
+            background-repeat: no-repeat;
+            background-size: cover; 
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <!-- here we the code for the Navigation Bar. -->
+    <div class="navbar">
+        <div class="logo">
+            <h3>LOGO</h3>
+            <div class="overlay"></div>
+        </div>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </div>
+
+    <div class="welcome">
+        <h1>WELCOME TO HARNIKKY <br> FASHION STORE.</h1>
+        <a href="">Shop Now >>></a>
+    </div>
+</div>
+    
+</body>
+</html>
