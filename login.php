3c7493f887de2638330a5ff09d3b0bd8d8034fe1
Amospelumi <oluwapelumiabiodun45@gmail.com>
2021-11-03 16:16:41 +0100

    update

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/store.css">
+    <title>Online-Store</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        body{
+            position: relative;
+            height : 50vh;
+            background-image: linear-gradient(#DA1396, #da139475); 
+            background-repeat: no-repeat;
+            background-size: cover; 
+        }
+        
+    </style>
+</head>
+<body>
+<div class="container">
+    <!-- here we the code for the Navigation Bar. -->
+    <div class="navbar">
+        <h3>LOGO</h3>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </div>
+
+    <div class="loginHeading">
+        <h2>Login <i class="fas fa-lock"></i></h2>
+    </div>
+
+    <div class="headline">
+        <h3>Kindly Login to your Account.</h3>
+    </div>
+    <div class="form">
+        <form action="index.php">
+            <label for="username">User Name:</label><br>
+            <input type="text" name="username" placeholder="Pel***"><br><br>
+
+            <label for="password">Password:</label><br>
+            <input type="password" name="password" placeholder="******"><br><br>
+
+            <input type="submit">
+        </form>
+    </div>
+
+    
+</div>
+    
+</body>
+</html>
